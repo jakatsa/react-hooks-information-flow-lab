add68b78d5677c50238aa9449d1a440891a73383
Joe <jakatsa5@gmail.com>
2024-02-07 14:28:36 +0300

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,24 +1,55 @@
 import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
 
-function Item({ name, category }) {
-  const [isInCart, setIsInCart] = useState(false);
+function ItemForm({ onItemFormSubmit }) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce");
 
-  function handleAddToCartClick() {
-    setIsInCart((isInCart) => !isInCart);
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    setCategory(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onItemFormSubmit({
+      id: uuid(),
+      name,
+      category,
+    });
   }
 
   return (
-    <li className={isInCart ? "in-cart" : ""}>
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button
-        className={isInCart ? "remove" : "add"}
-        onClick={handleAddToCartClick}
-      >
-        {isInCart ? "Remove From" : "Add to"} Cart
-      </button>
-    </li>
+    <form className="NewItem" onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+        />
+      </label>
+
+      <label>
+        Category:
+        <select
+          name="category"
+          value={category}
+          onChange={handleCategoryChange}
+        >
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+      </label>
+
+      <button type="submit">Add to List</button>
+    </form>
   );
 }
 
-export default Item;
+export default ItemForm;
